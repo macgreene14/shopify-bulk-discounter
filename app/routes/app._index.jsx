09873e0b17995839e28b227e2dcf91ffa4462fc3
jmg <macgreene14@gmail.com>
2023-09-19 14:05:08 -0600

    updated app index & readme

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,271 +1,173 @@
-import { useEffect } from "react";
-import { json } from "@remix-run/node";
-import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
-import {
-  Page,
-  Layout,
-  Text,
-  VerticalStack,
-  Card,
-  Button,
-  HorizontalStack,
-  Box,
-  Divider,
-  List,
-  Link,
-} from "@shopify/polaris";
-
+import { useState, useCallback } from "react";
+import { useActionData, useSubmit } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
+import { Card, Layout, Page, Select, PageActions } from "@shopify/polaris";
 
-export const loader = async ({ request }) => {
-  await authenticate.admin(request);
-
-  return null;
-};
-
+// Action gets triggerd on form submit
+// Update Variant pricing with new pricing by looping through product state and running mutation
 export async function action({ request }) {
   const { admin } = await authenticate.admin(request);
+  console.log("action triggered");
 
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
-  const response = await admin.graphql(
-    `#graphql
-      mutation populateProduct($input: ProductInput!) {
-        productCreate(input: $input) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
-                }
+  /** @type {any} */
+  const data = {
+    ...Object.fromEntries(await request.formData()),
+  };
+  console.log("data", typeof data, data);
+  const productJSON = JSON.parse(data.products);
+
+  // Loop through products and variants
+  for (const product of productJSON) {
+    console.log("product", typeof product, product); //json
+    // const productJSON = JSON.parse(product);
+
+    for (const variant of product.variants) {
+      const input = {
+        id: variant.id,
+        price: variant.newPrice, // Replace with the new price you want to set
+      };
+
+      // Call the GraphQL mutation with the input
+      const response = await admin.graphql(
+        `#graphql
+          mutation updateProductVariant($input: ProductVariantInput!) {
+            productVariantUpdate(input: $input) {
+              product {
+                id
+              }
+              productVariant {
+                id
+                price
+              }
+              userErrors {
+                field
+                message
               }
             }
-          }
+          }`,
+        {
+          variables: {
+            input,
+          },
         }
-      }`,
-    {
-      variables: {
-        input: {
-          title: `${color} Snowboard`,
-          variants: [{ price: Math.random() * 100 }],
-        },
-      },
+      );
+
+      const responseJson = await response.json();
+      console.log(responseJson);
     }
-  );
+  }
 
-  const responseJson = await response.json();
-
-  return json({
-    product: responseJson.data.productCreate.product,
-  });
+  // Return a response if needed
+  return { message: "Product variants updated successfully" };
 }
 
+// Main Component
 export default function Index() {
-  const nav = useNavigation();
-  const actionData = useActionData();
+  // State
+  const [selectedProducts, setSelectedProducts] = useState();
+  const [discountSelect, setDiscountSelect] = useState("0.10"); //defaults to 10% discount
   const submit = useSubmit();
+  const errors = useActionData() || {};
 
-  const isLoading =
-    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
-
-  const productId = actionData?.product?.id.replace(
-    "gid://shopify/Product/",
-    ""
+  // Handle Dropdown Input
+  const handleSelectChange = useCallback(
+    (value) => setDiscountSelect(value),
+    []
   );
 
-  useEffect(() => {
-    if (productId) {
-      shopify.toast.show("Product created");
-    }
-  }, [productId]);
+  // Handle AddDiscount Btn
+  const handleAddDiscount = async () => {
+    selectedProducts.forEach((product) => {
+      product.variants.forEach((variant) => {
+        const discount = parseFloat(discountSelect);
+        const originalPrice = parseFloat(variant.price);
+        const discountedPrice = originalPrice * (1 - discount);
+        variant.newPrice = discountedPrice.toFixed(2);
+      });
+    });
 
-  const generateProduct = () => submit({}, { replace: true, method: "POST" });
+    // submit products to backend for updating
+    const data = {
+      products: JSON.stringify(selectedProducts),
+    };
+    submit(data, { method: "post" });
+  };
+
+  // Pull up Resource Picker, set Product State
+  async function selectProducts() {
+    const products = await window.shopify.resourcePicker({
+      type: "product",
+      multiple: true,
+      selectionIds: selectedProducts,
+    });
+
+    if (products) {
+      // update state
+      setSelectedProducts(products);
+    } else {
+      // if no product selected, return
+      return;
+    }
+  }
 
   return (
     <Page>
-      <ui-title-bar title="Remix app template">
-        <button variant="primary" onClick={generateProduct}>
-          Generate a product
+      <ui-title-bar title="Bulk Discounter">
+        {/* <button variant="primary" onClick={() => navigate("/app/qrcodes/new")}> */}
+        <button variant="primary" onClick={selectProducts}>
+          Select Products
         </button>
       </ui-title-bar>
-      <VerticalStack gap="5">
-        <Layout>
-          <Layout.Section>
-            <Card>
-              <VerticalStack gap="5">
-                <VerticalStack gap="2">
-                  <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify app 🎉
-                  </Text>
-                  <Text variant="bodyMd" as="p">
-                    This embedded app template uses{" "}
-                    <Link
-                      url="https://shopify.dev/docs/apps/tools/app-bridge"
-                      target="_blank"
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    interface examples like an{" "}
-                    <Link url="/app/additional">
-                      additional page in the app nav
-                    </Link>
-                    , as well as an{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql"
-                      target="_blank"
-                    >
-                      Admin GraphQL
-                    </Link>{" "}
-                    mutation demo, to provide a starting point for app
-                    development.
-                  </Text>
-                </VerticalStack>
-                <VerticalStack gap="2">
-                  <Text as="h3" variant="headingMd">
-                    Get started with products
-                  </Text>
-                  <Text as="p" variant="bodyMd">
-                    Generate a product with GraphQL and get the JSON output for
-                    that product. Learn more about the{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql/latest/mutations/productCreate"
-                      target="_blank"
-                    >
-                      productCreate
-                    </Link>{" "}
-                    mutation in our API references.
-                  </Text>
-                </VerticalStack>
-                <HorizontalStack gap="3" align="end">
-                  {actionData?.product && (
-                    <Button
-                      url={`shopify:admin/products/${productId}`}
-                      target="_blank"
-                    >
-                      View product
-                    </Button>
-                  )}
-                  <Button loading={isLoading} primary onClick={generateProduct}>
-                    Generate a product
-                  </Button>
-                </HorizontalStack>
-                {actionData?.product && (
-                  <Box
-                    padding="4"
-                    background="bg-subdued"
-                    borderColor="border"
-                    borderWidth="1"
-                    borderRadius="2"
-                    overflowX="scroll"
-                  >
-                    <pre style={{ margin: 0 }}>
-                      <code>{JSON.stringify(actionData.product, null, 2)}</code>
-                    </pre>
-                  </Box>
-                )}
-              </VerticalStack>
-            </Card>
-          </Layout.Section>
-          <Layout.Section secondary>
-            <VerticalStack gap="5">
-              <Card>
-                <VerticalStack gap="2">
-                  <Text as="h2" variant="headingMd">
-                    App template specs
-                  </Text>
-                  <VerticalStack gap="2">
-                    <Divider />
-                    <HorizontalStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Framework
-                      </Text>
-                      <Link url="https://remix.run" target="_blank">
-                        Remix
-                      </Link>
-                    </HorizontalStack>
-                    <Divider />
-                    <HorizontalStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Database
-                      </Text>
-                      <Link url="https://www.prisma.io/" target="_blank">
-                        Prisma
-                      </Link>
-                    </HorizontalStack>
-                    <Divider />
-                    <HorizontalStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Interface
-                      </Text>
-                      <span>
-                        <Link url="https://polaris.shopify.com" target="_blank">
-                          Polaris
-                        </Link>
-                        {", "}
-                        <Link
-                          url="https://shopify.dev/docs/apps/tools/app-bridge"
-                          target="_blank"
-                        >
-                          App Bridge
-                        </Link>
-                      </span>
-                    </HorizontalStack>
-                    <Divider />
-                    <HorizontalStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        API
-                      </Text>
-                      <Link
-                        url="https://shopify.dev/docs/api/admin-graphql"
-                        target="_blank"
-                      >
-                        GraphQL API
-                      </Link>
-                    </HorizontalStack>
-                  </VerticalStack>
-                </VerticalStack>
-              </Card>
-              <Card>
-                <VerticalStack gap="2">
-                  <Text as="h2" variant="headingMd">
-                    Next steps
-                  </Text>
-                  <List spacing="extraTight">
-                    <List.Item>
-                      Build an{" "}
-                      <Link
-                        url="https://shopify.dev/docs/apps/getting-started/build-app-example"
-                        target="_blank"
-                      >
-                        {" "}
-                        example app
-                      </Link>{" "}
-                      to get started
-                    </List.Item>
-                    <List.Item>
-                      Explore Shopify’s API with{" "}
-                      <Link
-                        url="https://shopify.dev/docs/apps/tools/graphiql-admin-api"
-                        target="_blank"
-                      >
-                        GraphiQL
-                      </Link>
-                    </List.Item>
-                  </List>
-                </VerticalStack>
-              </Card>
-            </VerticalStack>
-          </Layout.Section>
-        </Layout>
-      </VerticalStack>
+      <Layout>
+        {selectedProducts && selectedProducts.length > 0 ? (
+          <Card>
+            <table>
+              <thead>
+                <tr>
+                  <th>Product Image</th>
+                  <th>Product Title</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedProducts.map((product) => (
+                  <tr key={product.id}>
+                    <td>
+                      <img
+                        src={product.images[0]?.originalSrc}
+                        alt={product.title}
+                        width="50"
+                      />
+                    </td>
+                    <td>{product.title}</td>
+                    <td>{product.variants[0].price}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Card>
+        ) : (
+          <p>No products selected.</p>
+        )}
+      </Layout>
+      <Layout.Section>
+        <Select
+          label="Discount Amount"
+          options={[
+            { label: "10%", value: "0.10" },
+            { label: "15%", value: "0.15" },
+            { label: "20%", value: "0.20" },
+          ]}
+          onChange={handleSelectChange}
+          value={discountSelect}
+        />
+        <PageActions
+          primaryAction={{
+            content: "Apply Discount",
+            onAction: handleAddDiscount,
+          }}
+        />
+      </Layout.Section>
     </Page>
   );
 }
